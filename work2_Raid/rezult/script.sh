c140ac7505075b7ebd46cb69e137845975a7be2a
Galkina Ekaterina <105000707+vaslin1904@users.noreply.github.com>
2022-05-30 20:28:15 +0500

    Push rezult of work2

--- work2_Raid/rezult/script.sh
+++ work2_Raid/rezult/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sudo su
+mkdir -p ~root/.ssh
+              cp ~vagrant/.ssh/auth* ~root/.ssh
+	      yum install -y mdadm smartmontools hdparm gdisk
 mdadm --zero-superblock --force /dev/sd{b,c}
-sudo mdadm --create --verbose /dev/md0 -l 1 -n 2 /dev/sd{b,c}
+mdadm --create --verbose /dev/md0 -l 1 -n 2 /dev/sd{b,c} <<EOF
+yes
+EOF
 mkdir /etc/mdadm/
 echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
 mdadm --detail --scan --verbose | awk '/ARRAY/{print}' >> /etc/mdadm/mdadm.conf
