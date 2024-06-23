8c23f304a36f5978db6a62579e748d650853937c
vaslin1904 <vaslin1904@yandex.ru>
2024-06-23 14:23:25 +0500

    Delete files

--- work14_logs/provision.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash!
-#Установка одинакового времени
-#Укажем часовой пояс (Московское время):
-cp /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-#Перезупустим службу NTP Chrony: 
-systemctl restart chronyd
